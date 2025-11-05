e4dc6816b271a26ab9f4277416025cab310287f8
Roslyn Daly <roslyndaly@kubrickgroup.com>
2025-11-05 16:05:36 +0000

    changes to main.py

--- currency_converter_task/currency_converter/main.py
+++ currency_converter_task/currency_converter/main.py
@@ -1,12 +1,21 @@
-import currency_converter as c
 
-from fetcher import fetch_exchange_rate
 from logger_config import my_logger
 from cli import cli
-from converter
+from converter import converter
+
+logger = my_logger()
 
 
 def main():
+
+    base, target, amount, mock = cli()
+
+    converted = converter(base,target,amount,mock)
+
+    print(f"{amount} {base} = {converted} {target}")
+
+    return converted
+
 
 if __name__ == "__main__":
     main()
