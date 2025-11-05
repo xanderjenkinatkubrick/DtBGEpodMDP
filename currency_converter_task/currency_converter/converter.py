6232748802da12d22248d97f58d2a521aecc01ec
Divine Osideko <divineosideko@kubrickgroup.com>
2025-11-05 15:25:36 +0000

    add logger and value error to convert function

--- currency_converter_task/currency_converter/converter.py
+++ currency_converter_task/currency_converter/converter.py
@@ -2,13 +2,26 @@
 Python file to convert money.
 """
 from fetcher import fetch_exchange_rate
+from logger_config import my_logger
 
-def converter(base: str, target: str, amount: float = 1, mock: bool = False) -> float:
+logger = my_logger()
+
+def converter(base: str, target: str, amount: float = 1, mock: bool = False) -> float|None:
     """
     Convert a given amount from base currency.
+    Returns converted amount (float) or None on conversion failure
     """
+    if amount <= 0:
+        logger.warning("Negative amount supplied. Please input a positive number.")
+        raise ValueError("Negative amount supplied. Please input a positive number.")
+
     rate = fetch_exchange_rate(base, target)
+    if rate is None:
+        logger.warning("Rate unavailable for conversion. Check spelling.")
+        return None
+
     converted = amount * rate
+    logger.info("Converted {amount} {base} to {target}")
 
     print(f"{amount} {base} = {converted} {target}")
     return converted
