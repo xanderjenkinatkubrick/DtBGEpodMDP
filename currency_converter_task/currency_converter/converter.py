ba32652788e8f526f7f129c5b2b9fc6f4b8fefb0
Divine Osideko <divineosideko@kubrickgroup.com>
2025-11-05 14:58:57 +0000

    create conversion function

--- currency_converter_task/currency_converter/converter.py
+++ currency_converter_task/currency_converter/converter.py
@@ -1,3 +1,14 @@
 """
 Python file to convert money.
 """
+from fetcher import fetch_exchange_rate
+
+def converter(base: str, target: str, amount: float = 1, mock: bool = False) -> float:
+    """
+    Convert a given amount from base currency.
+    """
+    rate = fetch_exchange_rate(base, target)
+    converted = amount * rate
+
+    print(f"{amount} {base} = {converted} {target}")
+    return converted
