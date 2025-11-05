39873efb082423ea8274577ad53593b73da09f82
Bruke Workneh <brukeworkneh@kubrickgroup.com>
2025-11-05 14:34:30 +0000

    CLI

---
+++ currency_converter_task/currency_converter/cli.py
@@ -0,0 +1,34 @@
+"""
+This should handle command line arguments
+"""
+
+import argparse
+
+# Create ArgumentParser object
+parser = argparse.ArgumentParser()
+
+# Add arguments
+parser.add_argument('--base', '-bc',
+                    nargs = 1,
+                    help='Base currency')
+parser.add_argument('--target', '-tc',
+                    nargs = 1,
+                    help='Target currency')
+parser.add_argument('--amount', '-a',
+                    nargs = '?',
+                    default = 1,
+                    help = 'Amount that should be exchanged')
+# parser.add_argument('--mock', '-mk',
+#                     nargs = '?',
+#                     default = 1,
+#                     help = 'Mock')
+
+args = parser.parse_args()
+
+# Access argument values
+base_c = args.base_currency
+target_c = args.target_currency
+
+
+if __name__ == '__main__':
+    print(f'base currency: {base_c} \ntarget currency: {target_c}')
